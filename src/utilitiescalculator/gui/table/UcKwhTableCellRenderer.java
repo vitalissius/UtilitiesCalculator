ae73b947085f112c14d9ff36fe503127ec7e7c8c
Vitalii <vitalii.skliarov.iv@gmail.com>
2018-06-28 18:07:39 +0300

    Elec boundary setting from constructor

--- src/utilitiescalculator/gui/table/UcKwhTableCellRenderer.java
+++ src/utilitiescalculator/gui/table/UcKwhTableCellRenderer.java
@@ -5,13 +5,20 @@ import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
-import utilitiescalculator.Settings;
 
 public class UcKwhTableCellRenderer extends DefaultTableCellRenderer {
 
     private static final Color BACKGROUND_COLOR_RED = new Color(255, 128, 128);
     private static final Color BACKGROUND_COLOR_WHITE = Color.WHITE;
-    private final int ELEC_BOUNDARY = Settings.INSTANCE.getElecBoundary();
+    private final int elecBoundary;
+
+    public UcKwhTableCellRenderer() {
+        elecBoundary = 100;
+    }
+
+    public UcKwhTableCellRenderer(int elecBoundary) {
+        this.elecBoundary = elecBoundary;
+    }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
@@ -19,7 +26,7 @@ public class UcKwhTableCellRenderer extends DefaultTableCellRenderer {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
         if (!isSelected) {
             Integer iValue = (Integer) value;
-            if (iValue > ELEC_BOUNDARY) {
+            if (iValue > elecBoundary) {
                 setBackground(BACKGROUND_COLOR_RED);
             } else {
                 setBackground(BACKGROUND_COLOR_WHITE);
